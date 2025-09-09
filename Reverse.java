4b36cb46d77803dd056c0b15fc28ce5b4cbbd1ba
Dhyan Dileep <136983069+DhyanDileep@users.noreply.github.com>
2025-09-09 19:49:48 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Reverse{
+    public static void main(String[] args) throws IOException{
+        DataInputStream obj=new DataInputStream(System.in);
+        System.out.println("Enter limit");
+        int limit=Integer.parseInt(obj.readLine());
+        int array[]=new int[limit];
+        System.out.println("Enter elements");
+        for(int i=0;i<limit;i++){
+            array[i]=Integer.parseInt(obj.readLine());
+        }
+        for(int i=limit-1;i>=0;i--){
+            System.out.println(array[i]+" ");
+        }
+    }
+}
